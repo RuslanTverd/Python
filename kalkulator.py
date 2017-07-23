395203d39568fc594d65c1c8c846b09f9e74d9ab
RuslanTverd <ruslan.tverdokhlib@gmail.com>
2017-07-23 16:42:00 +0300

    2 commit

--- kalkulator.py
+++ kalkulator.py
@@ -1,24 +1,24 @@
 print("\tКалькулятор\n")
 print("Программа поддерживает следующие операции:\n")
-print("\tсложение")
-print("\tвычитание")
-print("\tумножение")
-print("\tделение\n")
+print("\tсложение - \"+\"")
+print("\tвычитание - \"-\"")
+print("\tумножение - \"*\"")
+print("\tделение - \"/\"\n")
 
-int_1 = int(input("Введеите число:"))
+int_1 = float(input("Введеите число:"))
 operation = input("Введите операцию:")
-int_2 = int(input("Введите второе число:"))
+int_2 = float(input("Введите второе число:"))
 print(" ")
-if operation == "сложение":
+if operation == "+":
     otvet = int_1 + int_2
     print("Ваш ответ:", otvet)
-elif operation == "вычитание":
+elif operation == "-":
     otvet = int_1 - int_2
     print("Ваш ответ:", otvet)
-elif operation == "умножение":
+elif operation == "*":
     otvet = int_1 * int_2
     print("Ваш ответ:", otvet)
-elif operation == "деление":
+elif operation == "/":
     otvet = int_1 / int_2
     print("Ваш ответ:", otvet)
 else:
