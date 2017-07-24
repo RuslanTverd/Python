2b07490cc7c792ecbb5c5b46be834107744e46be
RuslanTverd <ruslan.tverdokhlib@gmail.com>
2017-07-24 10:49:50 +0300

    6 commit

--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
-def operation():
-    operation = 0
-    print(" ")
-    otv = float(input("Введеите число:"))
-    while operation != "q":
-        operation = input("Введите операцию:")
-        if operation == "+":
-            int_1 = float(input("Введеите число:"))
-            otv += int_1
+import unittest
 
-if  __name__ ==  "__main__" :
+class TestStringMethods(unittest.TestCase):
+
+  def test_upper(self):
+      operation = 0
+      print(" ")
+      otv = float(input("Введеите число:"))
+      while operation != "q":
+          operation = input("Введите операцию:")
+          if operation == "+":
+              int_1 = float(input("Введеите число:"))
+              otv += int_1
+
+if __name__ == '__main__':
+    unittest.main()
+
