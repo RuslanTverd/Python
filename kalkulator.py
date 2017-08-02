eca3ffdd2ee05294108899db904aa6060fb25eed
RuslanTverd <ruslan.tverdokhlib@gmail.com>
2017-08-02 12:53:36 +0300

    8 commit

--- kalkulator.py
+++ kalkulator.py
@@ -7,23 +7,37 @@ print("\tДеление - \"/\"")
 print("\tРавно - \"=\"")
 print("\tВыход - \"q\"\n")
 
-operation = 0
+
 print(" ")
+
+operation = None
 otv = float(input("Введеите число:"))
 while operation != "q":
     operation = input("Введите операцию:")
     if operation == "+":
         int_1 = float(input("Введеите число:"))
-        otv += int_1
+        def plus():
+            plus = otv + int_1
+            return plus
+        otv = plus()
     elif operation == "-":
         int_1 = float(input("Введеите число:"))
-        otv -= int_1
+        def minus():
+            minus = otv - int_1
+            return minus
+        otv = minus()
     elif operation == "*":
         int_1 = float(input("Введеите число:"))
-        otv *= int_1
+        def multiply():
+            multiply = otv * int_1
+            return multiply
+        otv = multiply()
     elif operation == "/":
         int_1 = float(input("Введеите число:"))
-        otv /= int_1
+        def division():
+            division = otv / int_1
+            return division
+        otv = division()
     elif operation == "q":
         print("")
     elif operation == "=":
